b7dad03317984b1f165bd8713f01daaf6685ca57
mahadinina <mh952219@gmail.com>
2023-10-21 16:52:53 +0600

    Login system and file authorized index file serve implemeted

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const session = require('express-session');
 const app = express();
-
-
-// Firebase Firestore
 
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountkey.json');
@@ -12,21 +10,75 @@ admin.initializeApp({
 });
 
 
+app.use(session({
+    secret: 'your-secret-key',
+    resave: true,
+    saveUninitialized: false,
+}));
 
-//  URL Ussage 
+app.use(express.json());
 
+app.use((req, res, next) => {
+    if (req.session && req.session.user) {
+        // User is logged in
+        res.locals.user = req.session.user;
+    } else {
+        res.locals.user = null;
+    }
+    next();
+});
 
-app.get('/', (req, res) => {
-    const index = fs.readFileSync('./html/index.html', 'utf-8');
-    res.send(index);
+app.post('/setSession', (req, res) => {
+    if (req.body.user) {
+        req.session.user = req.body.user;
+        req.session.isLoggedIn = true;
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
 });
 
 
-app.get('/train-info',(req,res)=>{
-    const train_info = fs.readFileSync('./html/train-search.html','utf-8')
-    res.send(train_info)
-});    
-    
+
+// URL Usage
+
+app.get('/', (req, res) => {
+    if (res.locals.user) {
+        const index2 = fs.readFileSync('./html/index2.html', 'utf-8');
+        res.send(index2);
+    } else {
+        const index = fs.readFileSync('./html/index.html', 'utf-8');
+        res.send(index);
+    }
+});
+
+app.get('/train-info', (req, res) => {
+    const train_info = fs.readFileSync('./html/train-search.html', 'utf-8');
+    res.send(train_info);
+});
+
+app.get('/login', (req, res) => {
+    const login = fs.readFileSync('./html/login.html', 'utf-8');
+    res.send(login);
+});
+
+
+app.post('/logout', (req, res) => {
+
+    req.session.destroy((err) => {
+        if (err) {
+            console.error('Session destroy error:', err);
+        }
+        res.sendStatus(200);
+    });
+});
+
+app.get('/login', (req, res) => {
+    const login = fs.readFileSync('./html/login.html', 'utf-8');
+    res.send(login);
+});
+
+
 
 // Backend
 
@@ -51,16 +103,20 @@ app.get('/getTrainsName', async (req, res) => {
 });
 
 
-// Testing
 
+
+
+// Testing
 app.get('/test', (req, res) => {
     const test = fs.readFileSync('./html/test.html', 'utf-8');
     res.send(test);
 });
 
 
+// Last
+
 app.get('/*', (req, res) => {
-    res.status(404).send("<h1>404 Not Found </h1> <a href='/'>Go back home </h1>");
+    res.status(404).send("<h1>404 Not Found</h1> <a href='/'>Go back home</a>");
 });
 
 
